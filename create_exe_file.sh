85c3dbb94a9656ec975abcd8bee27209964cd951
Le0v1n <67271439+Le0v1n@users.noreply.github.com>
2023-10-11 09:40:23 +0800

    Update create_exe_file.sh
    Update -> 1.2

--- create_exe_file.sh
+++ create_exe_file.sh
@@ -1,1 +1,1 @@
-pyinstaller --onefile --noconsole --icon file_icon.ico --name reformat-text-for-clips-ver-1.1 reformat-text-for-clips.py
+pyinstaller --onefile --noconsole --icon file_icon.ico --name reformat-text-for-clips-ver-1.2 reformat-text-for-clips.py
